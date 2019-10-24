e25475661b1e9341f9aaf97b5665e0c5b8219043
Joshua S. Carter <upsetitguy@gmail.com>
2019-10-23 20:14:18 -0500

    New version of website project

---
+++ Models/Collection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButterflyWebsite.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public string LatinName { get; set; }
+        public DateTime CatchDate { get; set; }
+
+        public string Sex { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Accuracy { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
